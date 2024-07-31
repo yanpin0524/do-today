7a986545f39a2d72ba24d429daa626ca5c9b17af
Yanpin Chouli <a131234568@gmail.com>
2024-07-31 11:24:55 +0800

    ✨ feat: 新增 Login, Signup 頁面

---
+++ src/pages/Signup.vue
@@ -0,0 +1,50 @@
+<template>
+  <BaseCard class="mt-5 p-4 w-75 mx-auto border rounded-5">
+    <form class="border-bottom border-success">
+      <h1 class="card-title mb-3 p-2 pb-3 text-success fw-bold">Sign Up</h1>
+      <div class="container">
+        <label for="user-name">User Name</label>
+        <input
+          class="form-control mb-3"
+          type="text"
+          id="user-name"
+          placeholder="Your Name ..."
+          v-model="userName" />
+
+        <label for="email">Email</label>
+        <input
+          class="form-control mb-3"
+          type="email"
+          id="email"
+          placeholder="Your Email ..."
+          v-model="email" />
+
+        <label for="password">Password</label>
+        <input
+          class="form-control mb-4"
+          type="password"
+          id="password"
+          placeholder="Your Password ..."
+          v-model="password" />
+        <button class="btn btn-lg btn-success w-100 mb-5">Sign up</button>
+      </div>
+    </form>
+    <div class="my-3 ps-2">
+      <RouterLink to="/auth/login" class="text-secondary">
+        Already have an account? <b>Login</b>
+      </RouterLink>
+    </div>
+  </BaseCard>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userName: "",
+      email: "",
+      password: "",
+    };
+  },
+};
+</script>
